8bc7cb46b1c27b1ec0fee3667ae7f34c47678ebf
Louis Callaway <Louis Callaway>
2017-10-02 13:44:48 +1100

    Add project files.

---
+++ WebApplication12/Data/WebApplication12Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication12.Models
+{
+    public class WebApplication12Context : DbContext
+    {
+        public WebApplication12Context (DbContextOptions<WebApplication12Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication12.Models.Movie> Movie { get; set; }
+    }
+}
